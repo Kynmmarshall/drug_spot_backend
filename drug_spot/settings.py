c0510255c1114324baa398a54b9f66d25c317c9d
Kynmmarshall <kynmmarshall@gmail.com>
2026-06-25 09:52:26 +0100

    update backend with firebase credential path

--- drug_spot/settings.py
+++ drug_spot/settings.py
@@ -121,3 +121,5 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+FIREBASE_CREDENTIALS_PATH = os.environ.get("FIREBASE_CREDENTIALS_PATH")
